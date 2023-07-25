256e7d075a4e9e444fce313edd74e6abdee6cf09
PaolaSoares <soaresgpaola@gmail.com>
2023-07-24 23:21:29 -0300

    Validação em algoritimo Luhn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,52 @@
-import validator from './validator.js';
+//import validator from "./validator";
+const form = document.querySelector('#form');
+const result = document.querySelector('.resultado');
 
-console.log(validator);
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const numCartao = form.numero.value;
+    const cvc = form.cvc.value;
+
+
+    if (validator.isValid(numCartao) && validator.isValid(cvc)) {
+        document.getElementById('result').innerText = 'Válido'
+    } else {
+        document.getElementById('result').result.innerText = 'Inválido';
+    }
+});
+    const validator = {
+        isValid(numCartao) {
+            //remove espaço em branco e verifica a entrada é uma string numérica válida
+            numCartao = numCartao.replace(/\s/g, '');
+            if (!/^\d+$/.test(numCartao)) {
+                return false;
+            }
+        
+        //Implementação do algoritmo de Luhn
+        let soma = 0;
+        let par = false;
+        for (let i = numCartao.length - 1; i >= 0; i--) {
+            let digit = parseInt(numCartao.charAt(i), 10);
+            if (par) {
+                digit *= 2;
+                if (digit > 9) {
+                    digit -= 9;
+                }   
+                    }
+            soma += digit;
+            par = !par;
+        }
+        return soma % 10 === 0;
+    },
+    maskify(numCartao) {
+        const ultimosDigit = numCartao.slice(-4);
+        const digitOculto = numCartao.slice(0, -4).replace(/./g, '#');
+        return ultimosDigit + digitOculto;
+    },
+    validaCvc(cvc){
+        const cvcVali = /^[0-9]{3,4}$/;
+        return cvcVali.test(cvc);
+    }
+
+};
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,28 @@
-const validator = {
-  // ...
-};
+// const validator = {
+//     isValid(numCartao) {
+//         //remove espaço em branco e verifica a entrada é uma string numérica válida
+//         numCartao = numCartao.replace(/\s/g, '');
+//         if (!/^\d+$/.test(numCartao)) {
+//             return false;
+//         }
+//         //Implementação do algoritmo de Luhn
+//         let soma = 0;
+//         let par = false;
+//         for (let i = numCartao.length - 1; i >= 0; i--) {
+//             let digit = parseInt(numCartao.charAt(i), 10);
+//             if (par) {
+//                 if ((digit *= 2) > 0) digit -= 9;
+//             }
+//             soma += digit;
+//             par = !par;
+//         }
+//         return soma % 10 === 0;
+//     },
+//     maskify(numCartao){
+//         const ultimosDigit = numCartao.slice(-4);
+//         const digitOculto = numCartao.slice(0, -4).replace(/./g,'#');
+//         return ultimosDigit + digitOculto;
+//     }
+// };
 
-export default validator;
+// export {validator};
