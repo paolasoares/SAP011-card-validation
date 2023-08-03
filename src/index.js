3c5d70d7b27212bbd5b04c0245a079a13008292b
PaolaSoares <soaresgpaola@gmail.com>
2023-08-03 15:45:30 -0300

    Atualizar

--- src/index.js
+++ src/index.js
@@ -3,45 +3,45 @@ const form = document.querySelector('#form');
 const result = document.querySelector('.resultado');
 const dadosFixos = document.querySelector('.dadosFixos');
 
-const dadosDigitados =[];
+const dadosDigitados = [];
 
 form.addEventListener('submit', function (e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    const numCartao = form.numero.value;
-    const data = form.data.value;
-    const cvc = form.cvc.value;
+  const numCartao = form.numero.value;
+  const data = form.data.value;
+  const cvc = form.cvc.value;
 
-    if (validator.isValid(numCartao)) {
-        const numFormato = validator.maskify(numCartao);
+  if (validator.isValid(numCartao)) {
+    const numFormato = validator.maskify(numCartao);
 
-        const msg = `
+    const msg = `
         Numero: ${numFormato}
         Data: ${data}
         Cvc: ${cvc}
         `;
-    
-        result.innerText = msg + `Válido` ;
 
-        dadosDigitados.push(numFormato+' - Válido');
-        salvaDados();
+    result.innerText = msg + `Válido`;
 
-    } else {
-        result.innerText =`Inválido`;
-        result.style.color = 'red';
-    } 
+    dadosDigitados.push(numFormato + ' - Válido');
+    salvaDados();
+
+  } else {
+    result.innerText = `Inválido`;
+    result.style.color = 'red';
+  }
 });
 
-function salvaDados(){
-    dadosFixos.innerHTML = "";
+function salvaDados() {
+  dadosFixos.innerHTML = "";
 
-    dadosDigitados.forEach((dados) => {
-        const div = document.createElement("div");
-        div.innerText = dados;
-        dadosFixos.appendChild(div);
-    })
+  dadosDigitados.forEach((dados) => {
+    const div = document.createElement("div");
+    div.innerText = dados;
+    dadosFixos.appendChild(div);
+  })
 }
 // Inserir o número que desejamos validar. Use apenas caracteres numéricos (dígitos) no cartão para validar [0-9]. - ok
 // Constatar se o resultado é válido ou não. - ok
-// Ocultar todos os dígitos do número do cartão, excetuando os últimos 4 caracteres. -ok 
+// Ocultar todos os dígitos do número do cartão, excetuando os últimos 4 caracteres. -ok
 // Não deve poder inserir um campo vazio. - ok
--- src/validator.js
+++ src/validator.js
@@ -1,37 +1,37 @@
 //valida Cartão de crédito com o algoritmo luhn.
 const validator = {
-    isValid(numCartao) {
-        //remove espaço em branco e verifica a entrada é uma string numérica válida
-        numCartao = numCartao.replace(/\s/g, '');
-        if (!/^\d+$/.test(numCartao)) {
-            return false;
+  isValid(numCartao) {
+    //remove espaço em branco e verifica a entrada é uma string numérica válida
+    numCartao = numCartao.replace(/\s/g, '');
+    if (!/^\d+$/.test(numCartao)) {
+      return false;
+    }
+    //Implementação do algoritmo de Luhn
+    let soma = 0;
+    let par = false;
+    for (let i = numCartao.length - 1; i >= 0; i--) {
+      const digit = parseInt(numCartao.charAt(i), 10);
+      //dobra o valor do digito se ele estiver em uma posição par.
+      if (par) {
+        let dobro = digit * 2;
+        //Se o result for maior ou igual a 10, sibtrei por 9;
+        if (dobro >= 10) {
+          dobro -= 9;
         }
-        //Implementação do algoritmo de Luhn
-        let soma = 0;
-        let par = false;
-        for (let i = numCartao.length - 1; i >= 0; i--) {
-            const digit = parseInt(numCartao.charAt(i), 10);
-            //dobra o valor do digito se ele estiver em uma posição par.
-            if (par) {
-                let dobro = digit * 2;
-                //Se o result for maior ou igual a 10, sibtrei por 9;
-                if (dobro >= 10) {
-                    dobro -= 9;
-                }
-                soma += dobro;
-            } else {
-                soma += digit;
-            }
-            par = !par;
-        }
-        return soma % 10 === 0;
-    },
+        soma += dobro;
+      } else {
+        soma += digit;
+      }
+      par = !par;
+    }
+    return soma % 10 === 0;
+  },
 
-    //deixa visivel somente os últimos digitos
-    maskify(numCartao) {
-        const ultimosDigit = 4;
-        return "#".repeat(numCartao.length - ultimosDigit) + numCartao.slice(-ultimosDigit);
-    },
+  //deixa visivel somente os últimos digitos
+  maskify(numCartao) {
+    const ultimosDigit = 4;
+    return "#".repeat(numCartao.length - ultimosDigit) + numCartao.slice(-ultimosDigit);
+  },
 
 };
 export default validator;
